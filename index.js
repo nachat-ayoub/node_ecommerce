670c7b521306af23cc32c4d4228a557a88a4d6e3
venomVV <73206390+venomVV@users.noreply.github.com>
2021-06-02 22:02:26 +0100

    update port

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ app.delete('/product/:id', (req, res) => {
 
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
  console.log(`Server running on ${PORT}`)
